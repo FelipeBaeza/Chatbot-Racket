406797b9cf9ce80e517102c649eb045381ee3afa
FelipeBaeza <felipe.baeza.m@usach.cl>
2023-09-30 22:34:03 -0300

    TDA system
    Creacion del constructor del system

---
+++ TDA_system_20637464_BaezaMunoz.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(require "TDA_option_20637464_BaezaMunoz.rkt")
+(require "TDA_chatbot_20637464_BaezaMunoz.rkt")
+
+
+;------------------------------------------Constructor--------------------------------------------------
+#|
+Nombre: system.
+Dominio: name (string) X InitialChatbotCodeLink (Int) X chatbot.
+Recorrido: system.
+Descripcion: Función constructora que toma como argumento un name, un InitialChatbotCodeLink y una lista chatbot.
+Retorna una lista que representa un system. El chatbot es procesadas para eliminar duplicados antes de ser incluidas en la lista.
+|#
+
+(define (system name InitialChatbotCodeLink . chatbot)
+  (list name InitialChatbotCodeLink (eliminar-ids-duplicados chatbot) '() '() '() (current-seconds)))
