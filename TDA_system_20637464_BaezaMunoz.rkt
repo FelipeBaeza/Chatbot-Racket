0acda48c94a6b5fdeb1f29e0f6f4a180422c804e
FelipeBaeza <felipe.baeza.m@usach.cl>
2023-10-04 22:31:41 -0300

    modificar system-login
    cracion de una funcion

--- TDA_system_20637464_BaezaMunoz.rkt
+++ TDA_system_20637464_BaezaMunoz.rkt
@@ -96,5 +96,27 @@ names de la lista de usuarios del sistema.
             (current-seconds))))
 
 
+#|
+Nombre: system-login.
+Dominio: system x user.
+Recorrido: system.
+Descripcion: Función modificadora que permite iniciar una sesión en el sistema si es que existe el usuario y
+todavia no se iniciado sesion.
+|#
+
+(define (system-login system user)
+  (if (equal? (register (map car (get-system-user system)) user) #t)
+      (if (null? (get-login-user system))
+          (list (get-name-system system)
+                (get-codelink-system system)
+                (get-chatbot-system system)
+                (get-system-user system)
+                (cons user (get-login-user system))
+                (get-talk-system system)
+                (current-seconds))
+          system)
+      system))
+
+
 ;-------------------------------------------------Provide------------------------------------------------
 (provide (all-defined-out))
