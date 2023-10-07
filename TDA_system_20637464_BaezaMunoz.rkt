bc8d7499131683a2dcbd79c9ee31e71b97be389b
FelipeBaeza <felipe.baeza.m@usach.cl>
2023-10-06 22:49:13 -0300

    Creacion de system-talk-rec
    funcion que permite interactuar con el chatbot

--- TDA_system_20637464_BaezaMunoz.rkt
+++ TDA_system_20637464_BaezaMunoz.rkt
@@ -51,7 +51,99 @@ descripcion: Funcion selectora que obtiene la lista de los chatbots del sistema.
   (caddr system))
 
 
+#|
+Nombre: get-current-seconds.
+Dominio: system.
+Recorrido: Lista.
+descripcion: Funcion selectora que obtiene la lista donde se encuentra la fecha del sistema.
+|#
 
+
+(define (get-current-seconds system)
+  (list-ref system 6))
+
+
+#|
+Nombre: new-chatbot-message-palabra.
+Dominio: Option x message.
+Recorrido: option.
+Tipo-Recursion: Cola
+Descripcion: Funcion selectora que devuelve una opcion que sea igual a message entre la lista de opciones .
+|#
+
+
+(define (new-chatbot-message-palabra option message)
+  (define (comparador palabras clave)
+    (if (null? palabras)
+        #f
+        (if (equal? (car palabras) message)
+            (car palabras)
+            (comparador (cdr palabras) clave))))
+  
+  (if (equal? message (comparador (list-ref (car option) 4) message))
+      (list (caddr (car option)) (cadddr (car option)))
+      (new-chatbot-message-palabra (cdr option) message)))
+
+#|
+Nombre: new-buscar-chatbot.
+Dominio: list-chatbot x chatbot-clave.
+Recorrido: lista.
+Tipo-recursion: cola
+Descripcion: Funcion selectora que busca el chatbot que su id sea igual al de chatbot-clave.
+|#
+
+
+
+(define (new-buscar-chatbot list-chatbot chatbot-clave)
+  (if (= (caar list-chatbot) (car chatbot-clave))
+      (busqueda-especifica (list-ref (car list-chatbot) 4) (cadr chatbot-clave))
+      (new-buscar-chatbot (cdr list-chatbot) chatbot-clave)))
+
+#|
+Nombre: busqueda-especifica.
+Dominio: list-elementos x clave .
+Recorrido: elemento.
+Tipo-recursion: cola
+Descripcion: Función selectora que busca un elemento de una lista de elemento que sea igual a clave.
+|#
+
+
+(define (busqueda-especifica List-elementos clave)
+    (if (= (caar List-elementos)  clave)
+       (car List-elementos)
+      (busqueda-especifica (cdr List-elementos) clave)))
+
+
+#|
+Nombre: new-chatbot-message.
+Dominio: option x clave.
+Recorrido: lista.
+Tipo-recursion: cola
+Descripcion: Función selectora que busca entre las opciones el id que sea igual a clave.
+|#
+
+
+
+(define (new-chatbot-message option clave)
+  (if (= (caar option)  clave)
+      (list (caddr (car option)) (cadddr (car option)))
+      (new-chatbot-message (cdr option) clave)))
+
+
+;-----------------------------------------------Pertenencia------------------------------------------------
+
+#|
+Nombre: is-number?.
+Dominio: str.
+Recorrido: booleano.
+Descripcion: Función de pertenencia que verifica si es un numero o un string.
+|#
+
+
+(define (is-number? str)
+  (if (string->number str)
+      #t  
+      #f)) 
 
 
 ;-----------------------------------------------Modificadores-------------------------------------------
@@ -124,7 +216,7 @@ Dominio: system.
 Recorrido: system.
 Descripcion: Función modificadora que permite cerrar una sesion abierta.
 |#
-;listo
+
 (define (system-logout system)
   (list (get-name-system system)
         (get-codelink-system system)
@@ -134,6 +226,72 @@ Descripcion: Función modificadora que permite cerrar una sesion abierta.
         (get-talk-system system)
         (current-seconds)))
 
+
+#|
+Nombre: agregar-contenido.
+Dominio: usuario x contenido x list-user-chathistory.
+Recorrido: lista.
+Tipo-recursion: cola
+Descripcion: Función modificadora que permite agregar contenido al chathistory.
+|#
+
+
+
+(define (agregar-contenido usuario contenido List-user-chathistory)
+  (cond
+    [(equal? (car (car List-user-chathistory)) usuario)
+     (cons (list usuario (append (cadr (car List-user-chathistory)) contenido))
+           (cdr List-user-chathistory))]
+    [else
+     (cons (car List-user-chathistory)
+           (agregar-contenido usuario contenido (cdr List-user-chathistory)))]))
+
+#|
+Nombre: system-talk-rec.
+Dominio: system x message.
+Recorrido: lista.
+Descripcion: Función modificadora que permite interactuar el usuario con el chatbot.
+|#
+
+
+(define (system-talk-rec system message)
+  (if (not (null? (get-login-user system)))
+      (if (null? (get-talk-system system));si todavia no se ha iniciado una coversacion
+          (list (get-name-system system)
+                (get-codelink-system system)
+                (get-chatbot-system system)
+                (agregar-contenido (car (get-login-user system)) (list-ref (busqueda-especifica (get-chatbot-system system) (get-codelink-system system)) 4)  (get-system-user system))
+                (get-login-user system)
+                (cons (list-ref (busqueda-especifica (get-chatbot-system system) (get-codelink-system system)) 4) (get-talk-system system))
+                (get-current-seconds system))
+          ;caso cuando entrega una palabra o un numero
+          (if (is-number? message)
+              (if (= (length (get-talk-system system)) 1);caso donde ya se mostro chatbot y se escoge entre chatbot1 y chatbot2
+                  (list (get-name-system system)
+                        (get-codelink-system system)
+                        (get-chatbot-system system)
+                        (agregar-contenido (car (get-login-user system)) (busqueda-especifica (list-ref (busqueda-especifica (get-chatbot-system system) (string->number message)) 4) (string->number message))  (get-system-user system))
+                        (get-login-user system)
+                        (cons (busqueda-especifica (list-ref (busqueda-especifica (get-chatbot-system system) (string->number message)) 4) (string->number message))  
+                              (get-talk-system system))
+                        (get-current-seconds system))
+                  (list (get-name-system system)
+                        (get-codelink-system system)
+                        (get-chatbot-system system)
+                        (agregar-contenido (car (get-login-user system)) (new-buscar-chatbot (get-chatbot-system system) (new-chatbot-message (caddr (car (get-talk-system system))) (string->number message)))  (get-system-user system))
+                        (get-login-user system)
+                        (cons (new-buscar-chatbot (get-chatbot-system system) (new-chatbot-message (caddr (car (get-talk-system system))) (string->number message)))
+                              (get-talk-system system))
+                        (get-current-seconds system)))
+              (list (get-name-system system)
+                    (get-codelink-system system)
+                    (get-chatbot-system system)
+                    (agregar-contenido (car (get-login-user system))  (new-buscar-chatbot (get-chatbot-system system) (new-chatbot-message-palabra (caddr (car (get-talk-system system))) message)) (get-system-user system))
+                    (get-login-user system)
+                    (cons (new-buscar-chatbot (get-chatbot-system system) (new-chatbot-message-palabra (caddr (car (get-talk-system system))) message))
+                          (get-talk-system system))
+                    (get-current-seconds system))))
+      system))
 
 ;-------------------------------------------------Provide------------------------------------------------
 (provide (all-defined-out))
